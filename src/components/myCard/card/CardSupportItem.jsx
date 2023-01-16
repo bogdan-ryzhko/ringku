fc2761bc74fa80bb8ddcb1a795af51f5947c14cd
Bogdan Ryzhko <m3meizumini@gmail.com>
2023-01-16 13:35:31 +0200

    add dropdown list in CardSupportItem

--- src/components/myCard/card/CardSupportItem.jsx
+++ src/components/myCard/card/CardSupportItem.jsx
@@ -1,8 +1,22 @@
 import React from "react";
 
 const CardSupportItem = ({ supportList }) => {
+	function onAnswer() {
+		const questions = document.querySelectorAll('.card-support__item');
+		const answers = document.querySelectorAll('.card-support__item-answer');
+
+		for (let i = 0; i < questions.length; i += 1) {
+			if (answers[i].classList.contains('card-support__item-answer--hidden')) {
+				questions[i].lastElementChild.classList.remove('card-support__item-answer--hidden');
+				questions[i].lastElementChild.classList.add('card-support__item-answer--visible');
+			} else if (answers[i].classList.contains('card-support__item-answer--visible')) {
+				questions[i].lastElementChild.classList.add('card-support__item-answer--hidden');
+				questions[i].lastElementChild.classList.remove('card-support__item-answer--visible');
+			}
+		}
+	}
 	return (
-		<li className="card-support__item">
+		<li onClick={onAnswer} className="card-support__item">
 			<p style={{borderLeft: `4px solid${supportList.color}`}} className="card-support__item-question">
 				{supportList.question}
 			</p>
